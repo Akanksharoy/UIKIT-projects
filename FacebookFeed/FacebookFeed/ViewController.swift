3570c582e21c97c96dd410e799f70d09cf84f3ce
Akanksha <akanksha.roy95@gmail.com>
2024-06-13 21:45:14 +0530

    first commit

---
+++ FacebookFeed/FacebookFeed/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  FacebookFeed
+//
+//  Created by Animesh on 25/05/24.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = UIColor(white: 0.95, alpha: 1.0)
+        // Do any additional setup after loading the view.
+    }
+}
