dad0a7586d3dd5c007875e3a7c61a0d1f02bed77
Akanksha <akanksha.roy95@gmail.com>
2025-04-11 12:53:00 +0530

    Conformed to protocol in Essential Feed

---
+++ EssentialFeed/EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -0,0 +1,33 @@
+//
+//  FeedItemsMapper.swift
+//  EssentialFeed
+//
+//  Created by Animesh on 06.04.2025.
+//
+
+class FeedItemsMapper {
+    
+    private struct Root: Decodable {
+        let items: [Item]
+        var feed:[FeedItem] {
+            return items.map{ $0.item }
+        }
+    }
+    private struct Item: Decodable {
+        let id: UUID
+        let description: String?
+        let location: String?
+        let image: URL
+        
+        var item: FeedItem {
+            return FeedItem(id: id, description: description, location: location, imageURL: image)
+        }
+    }
+    
+    static func map(data: Data, response:HTTPURLResponse)  -> LoadFeedResult {
+        guard response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
+            return .failure(RemoteFeedLoader.Error.invalidData)
+        }
+        return .success(root.feed)
+    }
+}
--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -5,34 +5,33 @@
 //  Created by Animesh on 05.04.2025.
 //
 
-public final class RemoteFeedLoader {
-     private let url: URL
-     private let client: HTTPClient
- 
+public final class RemoteFeedLoader: FeedLoader{
+    private let url: URL
+    private let client: HTTPClient
+    
     public enum Error: Swift.Error {
         case connectivity
         case invalidData
     }
     
-    public enum Result: Equatable {
-        case success([FeedItem])
-        case failure(Error)
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
     }
-     
-     public init(url: URL, client: HTTPClient) {
-         self.url = url
-         self.client = client
-     }
- 
-
-    public func load(completion: @escaping (Result) -> Void) {
-        client.get(from: url) { result in
+    
+    
+    public func load(completion: @escaping (LoadFeedResult) -> Void) {
+        client.get(from: url) { [weak self]  result in
+            guard let self = self else { return }
             switch result {
-            case .success:
-                completion(.failure(.invalidData))
+            case let .success(data, response):
+                completion(FeedItemsMapper.map(data: data, response: response))
             case .failure:
-                completion(.failure(.connectivity))
+                completion(.failure(Error.connectivity))
             }
         }
     }
- }
+}
+
+
+
